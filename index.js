806455c1e98d75fd42481e6ef069715f33645af9
ignacio.ambia <ignacio.ambia@gmail.com>
2022-03-08 22:39:47 -0600

    fix cors issue

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ require('./startup/prod')(app)
 const meals = require('./routes/meals');
 app.use('/meals', meals );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
+
 
 app.get('/', (req, res)=>{
  res.send('Hello world');
